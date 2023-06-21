4059469b46d53d66216c04efd89107240b74d70c
dunsd <dthomasparker@gmail.com>
2023-06-21 20:01:26 +0100

    Move react files to Client-App, initialise API files

---
+++ API/Models/Book.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public int Pages { get; set; }
+        public string? Review { get; set; }
+    }
+}
