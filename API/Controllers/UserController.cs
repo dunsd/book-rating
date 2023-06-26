e3d901820586298b7e43ca27e24fb16b9000daad
dunsd <dthomasparker@gmail.com>
2023-06-26 20:00:12 +0100

    Add regex for register, add getcurrentuser

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Models;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
@@ -7,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
+    
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
@@ -19,6 +20,7 @@ namespace API.Controllers
             _userManager = userManager;
         }
 
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO) {
             var user = await _userManager.FindByEmailAsync(loginDTO.Email);
@@ -30,19 +32,19 @@ namespace API.Controllers
             var result = await _userManager.CheckPasswordAsync(user, loginDTO.Password);
 
             if(result) {
-                return new UserDTO {
-                    DisplayName = user.DisplayName,
-                    Token = _tokenService.CreateToken(user),
-                    Username = user.UserName
-                };
+                return CreateUserObject(user);
             }
             return Unauthorized();
         }
         
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO) {
             if(await _userManager.Users.AnyAsync(x => x.UserName == registerDTO.Username)) {
                 return BadRequest("Username is taken");
+            }
+            if(await _userManager.Users.AnyAsync(x => x.Email == registerDTO.Email)) {
+                return BadRequest("Email is taken");
             }
             var user = new LocalUser {
                 DisplayName = registerDTO.DisplayName,
@@ -52,14 +54,30 @@ namespace API.Controllers
 
             var result = await _userManager.CreateAsync(user, registerDTO.Password);
 
-            if(result.Succeeded) {
-                return new UserDTO {
-                    DisplayName = user.DisplayName,
-                    Token = _tokenService.CreateToken(user),
-                    Username = user.UserName
-                };
+            if(result.Succeeded)
+            {
+                return CreateUserObject(user);
             }
             return BadRequest("Problem when creating new user");
+        }
+
+        
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDTO>> GetCurrentUser() {
+            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+
+        }
+        private ActionResult<UserDTO> CreateUserObject(LocalUser user)
+        {
+            return new UserDTO
+            {
+                DisplayName = user.DisplayName,
+                Token = _tokenService.CreateToken(user),
+                Username = user.UserName
+            };
         }
     }
 }
--- API/Models/RegisterDTO.cs
+++ API/Models/RegisterDTO.cs
@@ -1,15 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Models
 {
     public class RegisterDTO
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be more complex")]
         public string  Password { get; set; }
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
         public string Username { get; set; }
     }
 }
